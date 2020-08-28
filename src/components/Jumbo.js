65de8ab1d7f81fb8c6f5f41bf20b03bec0301c35
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-08-27 23:47:38 -0500

    :rocket create jumbo component

---
+++ src/components/Jumbo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyledJumbo } from '../../../Gatsby-platzi/src/styles/components'
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Consigue el mejor swag exclusivo y especial de Platzi</h2>
+        <small>Piezas elegantes para los mejores estudiantes</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+const styles = StyleSheet.create({})
