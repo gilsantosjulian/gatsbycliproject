36c589cf550a0aa00ff521e9cde8cc72da9ee374
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-08-31 21:30:18 -0500

    :hammer: add heart emoji

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { SEO } from '../components/seo'
-import { Button, Purchase } from '../components'
+import { SEO } from '../components'
+import { Button, Purchase } from '../styles/components'
 import { Link } from 'gatsby'
 
 export default function thanks() {
@@ -11,7 +11,7 @@ export default function thanks() {
         <h2>Compra Exitosa</h2>
         <p>Espero que disfrutes tu swag, lúcelo con orgullo</p>
         <p>!Te esperamos de vuelta, no pares de aprender!</p>
-        <span rol='img' aria-label='emoji'>Corazon</span>
+        <span rol='img' aria-label='emoji'>❤️</span>
         <Link to='/'>
           <Button>Volver al Catálogo</Button>
         </Link>
