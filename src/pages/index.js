644f034b83110912128c3c966790fefc9af00904
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-09-17 21:38:09 -0500

    :rocket: add styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import { Jumbo } from "../components"
 import { SEO } from "../components"
+import styled from 'styled-components'
 
 // The queries just can be add them on pages
 export const query = graphql`
@@ -18,10 +19,22 @@ export const query = graphql`
   }
 `
 
+const Button = styled.button`
+  width: 8rem;
+  background-color: #98ca3f;
+  border: none;
+  border-radius: 10px;
+  color: ${props => props.color};
+  &:hover {
+    transform: scale(1, 4);
+  }
+`
+
 const IndexPage = ({data}) => (
   <>
     <SEO title="Home" />
     <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
+    <Button color='gray'> Comprar</Button>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
