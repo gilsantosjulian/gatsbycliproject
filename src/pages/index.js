29fd52ca942d70fa52f15c940bef4f80c8376cc3
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-10-21 21:46:11 -0500

    :rocket: insert products component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import { Jumbo } from "../components"
-import { SEO } from "../components"
-import styled from 'styled-components'
+import { Products, SEO } from "../components"
 
 // The queries just can be add them on pages
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -16,32 +15,34 @@ export const query = graphql`
         }
       }
     }
+    allStripePrice {
+      edges {
+        node {
+          id
+          unit_amount
+          product {
+            name 
+            metadata {
+              img
+              description
+              wear
+            }
+          }
+        }
+      }
+    }
   }
 `
 
-const Button = styled.button`
-  width: 8rem;
-  background-color: #98ca3f;
-  border: none;
-  border-radius: 10px;
-  color: ${props => props.color};
-  &:hover {
-    transform: scale(1, 4);
-  }
-`
-
-const IndexPage = ({data}) => (
-  <>
-    <SEO title="Home" />
-    <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <Button color='gray'> Comprar</Button>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p><Link to="/page-2/">Go to page 2</Link></p>
-    <p><Link to="/thanks">Go to thanks</Link></p>
-    <p><Link to="/canceled">Go to canceled</Link></p>
-  </>
-)
+const IndexPage = ({data}) => {
+  console.log(data)
+  return (
+    <>
+      <SEO title="Home" />
+      <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
+      <Products products={data.allStripePrice.edges} />
+    </>
+  )
+}
 
 export default IndexPage
