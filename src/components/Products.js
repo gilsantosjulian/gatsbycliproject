3e8e0e76097bfdd325ab9cd70b3d1d0dfb5aa7f5
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-10-21 21:44:53 -0500

    :rocket: add Products component

---
+++ src/components/Products.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import priceFormat from '../utils/priceFormat'
+import { StyledProducts } from '../styles/components'
+
+export default function Product({products}) {
+  return (
+    <StyledProducts>
+      <h2>Products</h2>
+      <section>
+        {
+          products.map(({node}) => {
+            const price = priceFormat(node.unit_amount);
+            return (
+              <article key={node.id}>
+                <img src={node.product.metadata.img} alt={node.product.name}/>
+                <p>{node.product.name}</p>
+                <small>USD {price}</small>
+                <Link to={`/${node.id}`}>
+                  Comprar ahora <span>-</span>
+                </Link>
+              </article>
+            )
+          })
+        }
+      </section>
+    </StyledProducts>
+  )
+}
