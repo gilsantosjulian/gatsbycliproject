d7370150e71ac9f9f82cca2aed64f7883a24a9e1
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-10-28 23:07:29 -0500

    :rocket: use context

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,16 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useContext } from "react"
 import { MenuItem, StyledHeader } from '../styles/components'
+import { CartContext } from '../context'
 
 const logoImg = 'https://i.postimg.cc/6q3pg48v/Logo.png'
 const cartImg = 'http://i.postimg.cc/L6wpMxLt/cart.png'
 
-const Header = () => (
-  <StyledHeader>
+const Header = () => {
+  const { cart } = useContext(CartContext)
+  return (
+    <StyledHeader>
     <Link to='/'>
       <img 
         src={logoImg}
@@ -29,13 +32,16 @@ const Header = () => (
                 src={cartImg}
                 alt='cartLogo' 
               />
+              {cart.length}
             </span>
           </Link>
         </MenuItem>
       </ul>
     </nav>
   </StyledHeader>
-)
+  )
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
